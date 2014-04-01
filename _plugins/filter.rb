9699e05d1b9036cb75e1415b8e8fb2252c064725
Isaac Rosenberg <irosenb7@gmail.com>
2014-04-01 13:09:36 -0400

    converting

--- _plugins/filter.rb
+++ _plugins/filter.rb
@@ -6,8 +6,14 @@ module Jekyll
       doc = Nokogiri::HTML.fragment(html)
       links = doc.css('a')
       convert_links(links)
+      
+      h1s = doc.css('h1')
+      convert_h1s(h1s)
 
+      h2s = doc.css('h2')
+      convert_h2s(h2s)
 
+      doc.inner_html
     end
 
     private 
@@ -17,5 +23,18 @@ module Jekyll
         link['style'] = "color:#20b6fb;text-decoration:none;"
       end
     end
+
+    def convert_h1s(h1s)
+      h1s.map { |h1| h1['style'] = "font-size:27px;color:#939393;padding:10px 0px 17px 22px;font-weight:bold;line-height:22px;" }
+    end
+
+    def convert_h2s(h2s)
+      h2s.map { |h2| h2['style'] = "font-size:20px;color:#20b6fb;padding:10px 0px 17px 22px;font-weight:bold;line-height:22px;" }
+    end
+
+    
+
   end
 end
+
+Liquid::Template.register_filter(Jekyll::TableFilter)
