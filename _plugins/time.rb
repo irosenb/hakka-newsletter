177534d17c6f7d08b86b320611ed8912e82d770d
Isaac Rosenberg <irosenb7@gmail.com>
2014-04-03 11:45:57 -0400

    trying out other method

--- _plugins/time.rb
+++ _plugins/time.rb
@@ -1,23 +1,21 @@
 require "rest_client"
 require "json"
 require "nokogiri"
-require "pry"
 
 module Jekyll
   module TimeFilter
     def reading_time(link)
-      binding.pry
       response = JSON.parse(RestClient.get("http://reading-time.samrat.me/api?url=#{link}"))
       time = response['minutes'].round
-      
-      document = Nokogiri::HTML::Builder.new do |doc|
+      document = Nokogiri::HTML::DocumentFragment.parse ""
+      Nokogiri::HTML::Builder.with(document) do |doc|
         doc.span(:style => "color:grey;font-size:12px;font-weight:normal;") {
           doc.text "#{time}"
         }
         doc.br
       end
 
-      document.doc.inner_html
+      document.to_html
     end
   end
 end
